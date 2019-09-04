268a88fa6b871cc03444c91818dce1efc7301c33
BritishPandaChick <sbartley1080@gmail.com>
2019-09-04 18:26:23 -0400

    Add js file. Move stylesheets to assets folder. Add CSS styles for header and greeting section

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+function toggleNav(){
+  var x = document.querySelector(".myLinks");
+  if(x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
